a7f72abd3b641d5a20b0968a8eb3358fa9573622
artilleur <99259153+artilleur@users.noreply.github.com>
2023-11-21 10:00:52 +0100

    21112023  2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const Counter = () => {
+    const [count, setCount] = useState(0);
+  
+    const increment = () => {
+      setCount(count + 1);
+    };
+  
+    return (
+      <div>
+        <p>Compteur: {count}</p>
+        <button onClick={increment}>Incrémenter</button>
+      </div>
+    );
+  };
+
+
+ export default Counter;
